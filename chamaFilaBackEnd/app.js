6b613d53f566ec52300dcb5fc71a8cb0d4098bab
luisclaudiorp <61363429+luisclaudiorp@users.noreply.github.com>
2022-05-18 15:48:03 -0300

    tela de cadastro aplicaçao

--- chamaFilaBackEnd/app.js
+++ chamaFilaBackEnd/app.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
+const cors = require("cors")
 const express = require('express');
 const app = express();
 const pacientes = require('./src/controller/controllerPaciente')
 
 app.use(express.json())
+app.use(cors())
 app.use('/pacientes', pacientes)
 app.listen(process.env.PORT || 3001, () => console.log('Servidor rodando...'))
---
+++ chamaFilaFrontEnd/telaDeCadastro.js
@@ -0,0 +1,5 @@
+function pegarDados() {
+    const nome = document.getElementById("nome").value
+    const codigo = document.getElementById("codigo").value
+    alert(`Cliente cadastrado: ${nome} - ${codigo}`)
+}
